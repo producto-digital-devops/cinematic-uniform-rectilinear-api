00e37f350a5085cb091a08b55e77940681aa6e72
Jesus Aguilar <joac.aguilar@hotmail.com>
2022-03-27 21:06:37 -0500

    Calculate distance, velocity and time. Basic operations

--- calculate/__init__.py
+++ calculate/__init__.py
@@ -1,4 +1,7 @@
+from email import message
 import logging
+import json
+from unicodedata import decimal
 
 import azure.functions as func
 
@@ -6,19 +9,40 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    data = req.get_json()
+    variable = data["variable"]
+    params = data["parameters"]
+    result = 0
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    if variable == "distance":
+        velocity = params["velocity"]
+        time = params["time"]
+        result = calculate_distance(velocity, time)
+
+    if variable == "velocity":
+        distance = params["distance"]
+        time = params["time"]
+        result = calculate_velocity(distance, time)
+
+    if variable == "time":
+        velocity = params["velocity"]
+        distance = params["distance"]
+        result = calculate_time(velocity, distance)
+        
+    return func.HttpResponse(json.dumps({
+            "code": 200,
+            "message": "Executed successfully",
+            "data": result
+        }),
+        mimetype = "application/json",
+        status_code=200
+    )
+
+def calculate_distance(velocity: decimal, time: decimal):
+    return velocity*time
+
+def calculate_velocity(distance: decimal, time: decimal):
+    return distance / time
+
+def calculate_time(velocity: decimal, distance: decimal):
+    return distance / velocity
